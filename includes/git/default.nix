18dfa116796bed11887b6d1ec3c796884f3037b2
David McKay <rawkode@pm.me>
2018-12-12 12:29:25 +0000

    Enable gpg Signing for Git

--- includes/git/default.nix
+++ includes/git/default.nix
@@ -6,6 +6,15 @@
 
     userName  = "David McKay";
     userEmail = "rawkode@pm.me";
+
+    signing = {
+      key = "2D8641614CBEDB36443A1F1C4E81F61612CDF165";
+      signByDefault = true;
+    };
+
+    aliases = {
+      ps = "push";
+    };
   };
 
 }
