6c97e834c296f3f6cc6b9222ecb02389e45b2963
David McKay <rawkode@pm.me>
2018-12-13 12:11:31 +0000

    Upgrade on Install

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 sudo cp ./configuration.nix /etc/nixos/
 
-sudo nixos-rebuild switch
+sudo nix-channel --update
+sudo nixos-rebuild switch --upgrade
 
 sudo nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 sudo nix-channel --update
