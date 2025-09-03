bfa8191d3a7d401fc47b817192687c6d6ffe526e
David Flanagan <david@rawkode.dev>
2025-09-03 16:23:38 +0100

    home(desktop): make Firefox the default browser via xdg.mimeApps; stop Vivaldi claiming defaults

--- homeModules/desktop/firefox/default.nix
+++ homeModules/desktop/firefox/default.nix
@@ -162,4 +162,21 @@
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
   };
+
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = [ "firefox.desktop" ];
+    "x-scheme-handler/http" = [ "firefox.desktop" ];
+    "x-scheme-handler/https" = [ "firefox.desktop" ];
+    "x-scheme-handler/ftp" = [ "firefox.desktop" ];
+    "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+    "x-scheme-handler/about" = [ "firefox.desktop" ];
+    "x-scheme-handler/mailto" = [ "firefox.desktop" ];
+    "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+    "application/x-extension-htm" = [ "firefox.desktop" ];
+    "application/x-extension-html" = [ "firefox.desktop" ];
+    "application/x-extension-shtml" = [ "firefox.desktop" ];
+    "application/xhtml+xml" = [ "firefox.desktop" ];
+    "application/x-extension-xhtml" = [ "firefox.desktop" ];
+    "application/x-extension-xht" = [ "firefox.desktop" ];
+  };
 }
